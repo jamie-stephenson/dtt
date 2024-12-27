22decfebc4bf44fa26930f1ec374d2dfafc2f1c1
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-12-27 21:01:36 +0000

    Docker compose cluster for testing

--- env.sh
+++ env.sh
@@ -5,10 +5,8 @@ mount_dir="$1"
 
 #-PYTHON ENVIRONMENT--
 # If you want a specific python version you can use deadsnakes:
-sudo NEEDRESTART_MODE=l add-apt-repository -y ppa:deadsnakes/ppa
-sudo NEEDRESTART_MODE=l apt-get -o DPkg::Lock::Timeout=60 -y install python3.11
-sudo NEEDRESTART_MODE=l apt-get -o DPkg::Lock::Timeout=60 -y install python3.11-venv
-python3.11 -m venv ~/envs/dtt
+sudo NEEDRESTART_MODE=l apt-get -o DPkg::Lock::Timeout=60 -y install python3.12-venv
+python3 -m venv ~/envs/dtt
 source ~/envs/dtt/bin/activate
 pip install git+https://github.com/jamie-stephenson/dtt.git
 deactivate
