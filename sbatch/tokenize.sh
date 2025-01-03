1907b34a4e54e6db38d251b0f62d4723f82bb8fb
jamie-stephenson <jamie.t.stephenson@gmail.com>
2025-01-03 11:53:09 +0000

    Improved tokenized dataset handling
    Before:
    `tokens` path in path templates needed to be changed halfway through
    workflow because tokens were being saved to one path (on shared fs) and
    read from another (`~/data/`).
    
    Now:
    Tokens always saved to `~/data/` and copied across.
    
    This is still not ideal: the correct paths are hardcoded into
    `tokenize.sh` meaning changing path templates breaks this sbatch script.
    TODO: Maybe have `tokenize.sh` read path templates?

--- sbatch/tokenize.sh
+++ sbatch/tokenize.sh
@@ -11,4 +11,6 @@ source ~/envs/dtt/bin/activate
 # Train a tokenizer on a dataset AND use it to 
 # encode that same dataset  
 mpirun --bind-to none --mca btl_tcp_if_include eno1 dtt tokenize -c configs/config.yaml
-srun --ntasks=2 --ntasks-per-node=1 cp -r data/* ~/data/
+mkdir -p data
+mv ~/data data/
+srun --ntasks=$SLURM_NNODES --ntasks-per-node=1 cp -r data/* ~/data/
