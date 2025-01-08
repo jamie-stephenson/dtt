16b1946ae89cf633400a17eff339efb3b66bded9
jamie-stephenson <jamie.t.stephenson@gmail.com>
2025-01-08 13:53:06 +0000

    Moved `profile.py`

--- dtt/utils/__init__.py
+++ dtt/utils/__init__.py
@@ -1,7 +1,7 @@
 from .config import Config
 from .train import get_lr_scheduler, get_optimizer, get_dataloader
 from .dist import setup, cleanup
-from .profile import get_profiler
+from .train.profile import get_profiler
 
 __all__ = [
     "Config",
