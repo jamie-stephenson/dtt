da52fe305d716664177b60af7726569dba6f058e
jamie-stephenson <jamie.t.stephenson@gmail.com>
2025-01-10 19:48:00 +0000

    Weight tying and initialization
    Weight init required refactoring model

--- dtt/core/model.py
+++ dtt/core/model.py
@@ -38,13 +38,17 @@ class MultiHeadAttention(nn.Module):
     ):
         super().__init__()
         assert d_model % n_heads == 0, "d_model must be divisible by n_heads"
-        self.qkv_linear_layer = nn.Linear(
-            d_model, 3 * d_model, bias=False, device=device
-        )  # (B,T,C) -> (B,T,3C)
-        self.out_linear_layer = nn.Linear(d_model, d_model)  # (B,T,C) -> (B,T,C)
+
+        self.qkv = nn.Linear(d_model, 3 * d_model, bias=False, device=device)
+        # (B,T,C) -> (B,T,3C)
+
+        self.out = nn.Linear(d_model, d_model)
+        # (B,T,C) -> (B,T,C)
+
         self.attn_dropout = nn.Dropout(dropout)
         self.resid_dropout = nn.Dropout(dropout)
         self.n_heads = n_heads
+
         if mask_type == "causal":
             self.register_buffer(
                 "mask", torch.tril(torch.ones(n_ctx, n_ctx)).view(1, 1, n_ctx, n_ctx)
@@ -57,12 +61,12 @@ class MultiHeadAttention(nn.Module):
     def forward(self, x):
         B, T, C = x.shape  # B,T,C = batch size,`n_ctx`,`d_model`
 
-        q, k, v = self.qkv_linear_layer(x).chunk(3, dim=-1) # (B,T,3C) -> 3 lots of (B,T,C)
+        q, k, v = self.qkv(x).chunk(3, dim=-1)  # (B,T,3C) -> 3 lots of (B,T,C)
         q = q.view(B, T, self.n_heads, -1).transpose(1, 2)  # (B,T,C) -> (B,nh,T,C/nh)
         k = k.view(B, T, self.n_heads, -1).transpose(1, 2)  # (B,T,C) -> (B,nh,T,C/nh)
         v = v.view(B, T, self.n_heads, -1).transpose(1, 2)  # (B,T,C) -> (B,nh,T,C/nh)
 
-        attention_pattern = q @ k.transpose(-2, -1) / sqrt(C / self.n_heads)  
+        attention_pattern = q @ k.transpose(-2, -1) / sqrt(C / self.n_heads)
         # (B,nh,T,C/nh)*(B,nh,C/nh,T) -> (B,nh,T,T)
 
         attention_pattern = attention_pattern.masked_fill(
@@ -70,9 +74,15 @@ class MultiHeadAttention(nn.Module):
         )
         attention_pattern = self.attn_dropout(F.softmax(attention_pattern, dim=-1))
 
-        v_attended_to = attention_pattern @ v # (B,nh,T,T)*(B,nh,T,C/nh)  -> (B,nh,T,C/nh)
-        v_attended_to = v_attended_to.transpose(1, 2).contiguous().view(B, T, C) # (B,nh,T,C/nh) -> (B,T,C)
-        output = self.resid_dropout(self.out_linear_layer(v_attended_to))  # (B,T,C) -> (B,T,C)
+        v_attended_to = attention_pattern @ v
+        # (B,nh,T,T)*(B,nh,T,C/nh)  -> (B,nh,T,C/nh)
+
+        v_attended_to = v_attended_to.transpose(1, 2).contiguous().view(B, T, C)
+        # (B,nh,T,C/nh) -> (B,T,C)
+
+        output = self.resid_dropout(self.out(v_attended_to))
+        # (B,T,C) -> (B,T,C)
+
         return output
 
 
@@ -125,11 +135,15 @@ class TransformerBlock(nn.Module):
         return x
 
 
-def get_model(cfg):
-    # Note: no softmax on last layer, this model outputs logits ready for crossentropy loss.
-    model = nn.Sequential(
-        EmbeddingLayer(cfg.vocab_size, cfg.n_ctx, cfg.d_model, cfg.device, cfg.dropout),
-        *[
+class Transformer(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+        self.embed = EmbeddingLayer(
+            cfg.vocab_size, cfg.n_ctx, cfg.d_model, cfg.device, cfg.dropout
+        )
+
+        self.blocks = nn.Sequential(*[
             TransformerBlock(
                 cfg.n_ctx,
                 cfg.d_mlp,
@@ -140,10 +154,46 @@ def get_model(cfg):
                 cfg.mask_type,
             )
             for _ in range(cfg.n_blocks)
-        ],
-        nn.Linear(cfg.d_model, cfg.vocab_size),
-    )
+        ])
 
+        # Note: no softmax on last layer, this model outputs logits ready for crossentropy loss.
+        self.unembed = nn.Linear(cfg.d_model, cfg.vocab_size)
+
+        # init weights
+        self.apply(self._init_weights)
+
+        # weight tying
+        self.unembed.weight = self.embed.token_embedding.weight
+
+    def _init_weights(self, module):
+        if isinstance(module, EmbeddingLayer):
+            # As per https://github.com/karpathy/build-nanogpt/blob/master/train_gpt2.py
+            nn.init.normal_(module.token_embedding.weight, std=0.02)
+
+        elif isinstance(module, MultiHeadAttention):
+            nn.init.normal_(module.qkv.weight, std=1 / sqrt(module.qkv.in_features))
+            nn.init.normal_(module.out.weight, std=1 / sqrt(module.out.in_features))
+
+        elif isinstance(module, MLP):
+            # As per https://github.com/TransformerLensOrg/TransformerLens/blob/main/transformer_lens/HookedTransformer.py#L1381
+            nn.init.normal_(
+                module.layer_1.weight, std=1 / sqrt(module.layer_1.in_features)
+            )
+            nn.init.normal_(
+                module.layer_2.weight, std=1.57 / sqrt(module.layer_2.in_features)
+            )
+
+
+    def forward(self, x):
+        x = self.embed(x)
+        x = self.blocks(x)
+        x = self.unembed(x)
+
+        return x
+
+
+def get_model(cfg):
+    model = Transformer(cfg)
     torch.set_float32_matmul_precision("high")
 
     model = torch.compile(
